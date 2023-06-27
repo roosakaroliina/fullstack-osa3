a324e614b34a2ea424f342807faa0b573f14b8bf
Roosa Kuusivaara <roosa.kuusivaara@tuni.fi>
2023-06-27 14:53:54 +0300

    3.5. valmis

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
+
 let persons = [
     {
         id: 1,
@@ -29,27 +32,52 @@ app.get('/api/persons', (req, res) => {
 })
 
 app.get('/info', (req, res) => {
-    res.send('<p>Phonebook has info for ' + persons.length + ' people</p>' 
-    + '<p>' + new Date + ' </p>')
+    res.send('<p>Phonebook has info for ' + persons.length + ' people</p>'
+        + '<p>' + new Date + ' </p>')
 })
 
 app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     const person = persons.find(person => person.id === id)
-    
-  
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
-  })
 
-  app.delete('/api/persons/:id', (request, response) => {
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     persons = persons.filter(person => person.id !== id)
 
     response.status(204).end()
+})
+
+const generateRandomId = (max) => {
+    return Math.floor(Math.random() * max);
+
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+
+    const person = {
+        id: generateRandomId(200),
+        name: body.name,
+        number: body.number,
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
 })
 
 const PORT = 3001
