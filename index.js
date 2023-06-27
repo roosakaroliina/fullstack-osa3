cc704993e4c9b1fb688c958b9ac3a8f9ebcd4fc7
Roosa Kuusivaara <roosa.kuusivaara@tuni.fi>
2023-06-27 14:06:26 +0300

    3.3. valmis

--- index.js
+++ index.js
@@ -29,8 +29,21 @@ app.get('/api/persons', (req, res) => {
 })
 
 app.get('/info', (req, res) => {
-    res.send('<p>Phonebook has info for ' + persons.length + ' people</p>' + '<p>' + new Date + ' </p>')
+    res.send('<p>Phonebook has info for ' + persons.length + ' people</p>' 
+    + '<p>' + new Date + ' </p>')
 })
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    
+  
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
 
 const PORT = 3001
 app.listen(PORT, () => {
