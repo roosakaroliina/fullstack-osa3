7fbeec719ef1637b7281d0d78110267af928b085
Roosa Kuusivaara <roosa.kuusivaara@tuni.fi>
2023-06-27 15:02:29 +0300

    3.6. valmis

--- index.js
+++ index.js
@@ -68,6 +68,18 @@ app.post('/api/persons', (request, response) => {
             error: 'name missing'
         })
     }
+    if (!body.number) {
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    }
+
+    const foundPerson = persons.find(person => person.name === body.name)
+    if (foundPerson) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
 
     const person = {
         id: generateRandomId(200),
