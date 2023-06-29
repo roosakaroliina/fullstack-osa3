f121c78945b9b8b894e633abedf5169a84cccafe
Roosa Kuusivaara <roosa.kuusivaara@tuni.fi>
2023-06-29 14:04:04 +0300

    3.8. valmis

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
 
+morgan.token("data", request => {
+    return JSON.stringify(request.body); //Muuttaa json-muotoon
+  });
+
+
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :data"))
 
 let persons = [
     {
