8281071ae27cc6b0691d1ecc7464bb4eee0b2543
Roosa Kuusivaara <roosa.kuusivaara@tuni.fi>
2023-07-18 12:24:53 +0300

    errorhandlereiden paikan muutos

--- backend_puhelinluettelo/index.js
+++ backend_puhelinluettelo/index.js
@@ -15,22 +15,7 @@ const requestLogger = (request, response, next) => {
 }
 
 
-const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-}
 
-const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    }
-    else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
-
-    next(error)
-}
 
 morgan.token("data", request => {
     return JSON.stringify(request.body)
@@ -118,7 +103,23 @@ app.put('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+    else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
+
+    next(error)
+}
 app.use(errorHandler)
 
 const PORT = process.env.PORT
