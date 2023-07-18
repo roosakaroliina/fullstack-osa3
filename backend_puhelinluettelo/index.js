88773fc85fa0e118445548b9a9e9d4ac8e1f9df0
Roosa Kuusivaara <roosa.kuusivaara@tuni.fi>
2023-07-18 22:47:58 +0300

    unknownendpoint lisätty takas

--- backend_puhelinluettelo/index.js
+++ backend_puhelinluettelo/index.js
@@ -15,7 +15,22 @@ const requestLogger = (request, response, next) => {
 }
 
 
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
 
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+    else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
+
+    next(error)
+}
 
 morgan.token("data", request => {
     return JSON.stringify(request.body)
@@ -103,20 +118,7 @@ app.put('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-
-
-const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    }
-    else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
-
-    next(error)
-}
+app.use(unknownEndpoint)
 app.use(errorHandler)
 
 const PORT = process.env.PORT
